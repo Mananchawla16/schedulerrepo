0da193a44e984af266bceca376eb19975b48a7d1
ngupta16 <ngupta16@MacBook-Pro.local>
2022-06-15 23:03:46 +0530

    added gatling script

---
+++ app/src/main/resources/PopulateTestData.py
@@ -0,0 +1,6 @@
+import boto3
+
+def hit_scheduler_endpoint() :
+    print("=====================================\n")
+    print("Uploading offering is ->"+offering+" and service ->"+service+" and template ->"+template)
+    print("=====================================\n")
---
+++ perf-tests/src/test/scala/com/intuit/gatling/requests/Scheduler.scala
@@ -0,0 +1,20 @@
+package com.intuit.gatling.requests
+
+import com.intuit.gatling.common.Base
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+
+object Scheduler extends Base {
+
+  val CREATE_SCHEDULE = "create_scheduler"
+
+  def createDocument(): ChainBuilder = Request {
+    http(CREATE_SCHEDULE)
+      .post("v1/jobs/schedule-oinp-job")
+      .header("intuit_tid",session=>generateTID("schedulerLoadTest"))
+      .header("Authorization", basicPrivateAuthPlus)
+      .body(StringBody("""{"scheduleId":"test-schedule","triggerAt":"2022-06-15T23:30:00+01:00","triggerData": "{\"authId\":\"ghfhgj\",\"sourceObjectId\":\"ghhg\",\"sourceServiceId\":\"fhg\",\"creationDate\":\"gfhj\"}"}"""))
+      .check(jsonPath("$.id").saveAs("scheduleApiId"))
+      .check(status.is(200))
+  }
